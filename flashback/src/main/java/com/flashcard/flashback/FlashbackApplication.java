daa913e653ed9ed5ee923ecff3e9e0452b325e5b
paweu9000 <wolfenorrin5@gmail.com>
2023-01-25 17:41:32 +0100

    Setup db

---
+++ flashback/src/main/java/com/flashcard/flashback/FlashbackApplication.java
@@ -0,0 +1,13 @@
+package com.flashcard.flashback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashbackApplication.class, args);
+	}
+
+}
---
+++ flashback/src/test/java/com/flashcard/flashback/FlashbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashcard.flashback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
