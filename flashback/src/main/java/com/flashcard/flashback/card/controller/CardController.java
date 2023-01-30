b2fb459dd6bfd238ec25898a3af607b8d3050f73
paweu9000 <wolfenorrin5@gmail.com>
2023-01-30 17:07:10 +0100

    Initial commit CardController

---
+++ flashback/src/main/java/com/flashcard/flashback/card/controller/CardController.java
@@ -0,0 +1,10 @@
+package com.flashcard.flashback.card.controller;
+
+import com.flashcard.flashback.card.service.CardService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/cards")
+public record CardController(CardService cardService) {
+}
