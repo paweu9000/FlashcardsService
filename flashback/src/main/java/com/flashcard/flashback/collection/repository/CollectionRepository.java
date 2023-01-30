cd94216cc907cff8958a66986be227fb14c6c0fa
paweu9000 <wolfenorrin5@gmail.com>
2023-01-30 16:32:57 +0100

    Initial commit CollectionRepository, CollectionService

---
+++ flashback/src/main/java/com/flashcard/flashback/collection/repository/CollectionRepository.java
@@ -0,0 +1,9 @@
+package com.flashcard.flashback.collection.repository;
+
+import com.flashcard.flashback.collection.CollectionEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CollectionRepository extends JpaRepository<CollectionEntity, Long> {
+}
