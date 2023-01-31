c5cc248afbfad68c8ce56afb7d363bc59bf7017a
paweu9000 <wolfenorrin5@gmail.com>
2023-01-31 10:49:47 +0100

    UserService addred findByEmailOrLogin method, unwrapUser method

--- flashback/src/main/java/com/flashcard/flashback/user/service/UserService.java
+++ flashback/src/main/java/com/flashcard/flashback/user/service/UserService.java
@@ -1,8 +1,23 @@
 package com.flashcard.flashback.user.service;
 
+import com.flashcard.flashback.user.UsersEntity;
 import com.flashcard.flashback.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public record UserService(UserRepository userRepository) {
+
+    public UsersEntity findByEmailOrLogin(String emailOrLogin) {
+        UsersEntity optUser =  userRepository.findAll().stream().filter(usersEntity -> usersEntity.getLogin().equals(emailOrLogin) ||
+                usersEntity.getEmail().equals(emailOrLogin)).findFirst().get();
+        return unwrapUser(optUser);
+    }
+
+    public UsersEntity unwrapUser(UsersEntity user) {
+        if(user != null) {
+            return user;
+        } else {
+            throw new RuntimeException("User does not exist!");
+        }
+    }
 }
