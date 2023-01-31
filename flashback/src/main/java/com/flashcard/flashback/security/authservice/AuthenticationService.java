baf32e2309a913d561cddddea809b38c2d5cc764
paweu9000 <wolfenorrin5@gmail.com>
2023-01-31 22:04:39 +0100

    created new register method

--- flashback/src/main/java/com/flashcard/flashback/security/authservice/AuthenticationService.java
+++ flashback/src/main/java/com/flashcard/flashback/security/authservice/AuthenticationService.java
@@ -1,36 +1,57 @@
 package com.flashcard.flashback.security.authservice;
 
 import com.flashcard.flashback.user.entity.UsersEntity;
+import com.flashcard.flashback.user.role.Role;
 import com.flashcard.flashback.user.service.UserService;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
-@Service
-public class AuthenticationService implements UserDetailsService {
+@Component
+public class AuthenticationService implements AuthenticationProvider {
 
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthenticationService(UserService userService) {
+    public AuthenticationService(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        UsersEntity user = userService.findByEmailOrLogin(username);
+        if(passwordEncoder.matches(password, user.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(username, password, getGrantedAuthorities(user.getRole()));
+        } else {
+            throw new BadCredentialsException("Invalid credentials");
+        }
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(Role authorities) {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority(authorities.name()));
+        return grantedAuthorities;
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        String name, password;
-        List<GrantedAuthority> authorities;
-        UsersEntity user = userService.findByEmailOrLogin(username);
-        name = user.getUsername();
-        password = user.getPassword();
-        authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority(user.getRole().toString()));
-        return new User(name, password, authorities);
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
     }
 }
