3e8f5eb59232fa8e01761b5716267fada7942748
paweu9000 <wolfenorrin5@gmail.com>
2023-02-01 09:59:34 +0100

    initial commit collectionDAO

---
+++ flashback/src/main/java/com/flashcard/flashback/collection/data/CollectionDao.java
@@ -0,0 +1,20 @@
+package com.flashcard.flashback.collection.data;
+
+import com.flashcard.flashback.card.data.CardDao;
+import com.flashcard.flashback.card.entity.CardEntity;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class CollectionDao {
+
+    private Long likes;
+    private List<CardDao> cards = new ArrayList<>();
+
+    public CollectionDao(Long likes, List<CardEntity> cardEntities) {
+        this.likes = likes;
+        cardEntities.forEach(card -> this.cards.add(new CardDao(card)));
+    }
+}
