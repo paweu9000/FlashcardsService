350312e8f4fc2ef4248f1d2b959b584352838521
paweu9000 <wolfenorrin5@gmail.com>
2023-02-04 20:16:02 +0100

    collectionService toDao method

--- flashback/src/main/java/com/flashcard/flashback/collection/service/CollectionService.java
+++ flashback/src/main/java/com/flashcard/flashback/collection/service/CollectionService.java
@@ -1,8 +1,14 @@
 package com.flashcard.flashback.collection.service;
 
+import com.flashcard.flashback.collection.data.CollectionDao;
+import com.flashcard.flashback.collection.entity.CollectionEntity;
 import com.flashcard.flashback.collection.repository.CollectionRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public record CollectionService(CollectionRepository collectionRepository) {
+
+    public CollectionDao toDao(CollectionEntity collection) {
+        return new CollectionDao(collection);
+    }
 }
