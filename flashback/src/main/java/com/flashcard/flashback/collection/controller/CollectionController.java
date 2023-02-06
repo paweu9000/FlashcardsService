6882dfdd436c9673bfa59dced70be418937e8d7b
paweu9000 <wolfenorrin5@gmail.com>
2023-02-06 15:09:50 +0100

    findCollectionById method, check if collection exists

--- flashback/src/main/java/com/flashcard/flashback/collection/controller/CollectionController.java
+++ flashback/src/main/java/com/flashcard/flashback/collection/controller/CollectionController.java
@@ -1,10 +1,23 @@
 package com.flashcard.flashback.collection.controller;
 
+import com.flashcard.flashback.collection.data.CollectionDao;
+import com.flashcard.flashback.collection.entity.CollectionEntity;
 import com.flashcard.flashback.collection.service.CollectionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/collection")
 public record CollectionController(CollectionService collectionService) {
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CollectionDao> getCollection(@PathVariable Long id) {
+        CollectionEntity collection = collectionService.findById(id);
+        CollectionDao collectionDao = new CollectionDao(collection);
+        return new ResponseEntity<>(collectionDao, HttpStatus.OK);
+    }
 }
