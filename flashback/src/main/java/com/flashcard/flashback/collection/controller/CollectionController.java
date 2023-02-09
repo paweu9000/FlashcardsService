f59a2b08f9f9c211ba2c15e9a4c0246c286080e5
paweu9000 <wolfenorrin5@gmail.com>
2023-02-09 19:11:42 +0100

    created deleteCollectionById

--- flashback/src/main/java/com/flashcard/flashback/collection/controller/CollectionController.java
+++ flashback/src/main/java/com/flashcard/flashback/collection/controller/CollectionController.java
@@ -5,10 +5,7 @@ import com.flashcard.flashback.collection.entity.CollectionEntity;
 import com.flashcard.flashback.collection.service.CollectionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/collection")
@@ -19,5 +16,11 @@ public record CollectionController(CollectionService collectionService) {
         CollectionEntity collection = collectionService.findById(id);
         CollectionDao collectionDao = new CollectionDao(collection);
         return new ResponseEntity<>(collectionDao, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteCollection(@PathVariable Long id) {
+        collectionService.deleteCollectionById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
