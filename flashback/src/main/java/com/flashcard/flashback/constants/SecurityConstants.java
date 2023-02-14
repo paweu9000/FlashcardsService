f44357ded6f48f101455680c0029c313e2a7b16f
paweu9000 <wolfenorrin5@gmail.com>
2023-02-14 15:36:16 +0100

    initial JWTAuthorizationFilter

--- flashback/src/main/java/com/flashcard/flashback/constants/SecurityConstants.java
+++ flashback/src/main/java/com/flashcard/flashback/constants/SecurityConstants.java
@@ -4,7 +4,7 @@ public record SecurityConstants (){
 
     //SET TO ENV VARIABLES LATER
     public static final int TOKEN_EXPIRATION_TIME = 7200000;
-    public static final String BEARER = "Bearer";
+    public static final String BEARER = "Bearer ";
     public static final String AUTHORIZATION = "Authorization";
     public static final String REGISTER_PATH = "/api/auth/register";
     public static final String SECRET_KEY = "+KbPeShVmYq3t6w9z$C&F)J@McQfTjWnZr4u7x!A%D*G-KaPdRgUkXp2s5v8y/B?";
