4696cd6938910d68d5eb93a2d337c15451e6ae6d
paweu9000 <wolfenorrin5@gmail.com>
2023-02-27 14:55:44 +0100

    wiremock TestGetAllCardsRequest

--- flashback/src/test/java/com/flashcard/flashback/wiremock/CardControllerTests.java
+++ flashback/src/test/java/com/flashcard/flashback/wiremock/CardControllerTests.java
@@ -4,6 +4,8 @@ import com.flashcard.flashback.card.entity.CardEntity;
 import com.flashcard.flashback.card.repository.CardRepository;
 import com.flashcard.flashback.card.service.CardService;
 import com.flashcard.flashback.collection.entity.CollectionEntity;
+import com.flashcard.flashback.collection.repository.CollectionRepository;
+import com.flashcard.flashback.collection.service.CollectionService;
 import com.flashcard.flashback.user.entity.UsersEntity;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
@@ -16,10 +18,9 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 import java.io.IOException;
-import java.net.http.HttpClient;
-import java.net.http.HttpResponse;
 import java.util.Optional;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
@@ -32,6 +33,13 @@ public class CardControllerTests {
     @Mock
     CardRepository cardRepository;
 
+    @Mock
+    CollectionRepository collectionRepository;
+
+    @InjectMocks
+    @Spy
+    CollectionService collectionService;
+
     @InjectMocks
     CardService cardService;
     CollectionEntity collection;
@@ -40,12 +48,16 @@ public class CardControllerTests {
     @Before
     public void setUp() {
         MockitoAnnotations.openMocks(this);
+        cardService.setCollectionService(collectionService);
         wireMockServer = new WireMockServer(8080);
         wireMockServer.start();
         user = new UsersEntity();
         user.setId(1L);
+        user.setLogin("login");
+        user.setEmail("email@example.com");
         collection = new CollectionEntity();
         collection.setId(1L);
+        user.addCollection(collection);
     }
 
     @After
@@ -77,6 +89,26 @@ public class CardControllerTests {
         CloseableHttpResponse response = client.execute(request);
 
         assertEquals(404, response.getCode());
+    }
+
+    @Test
+    public void testGetAllCardsRequest() throws IOException {
+        CardEntity card1 = new CardEntity(1L, "Side", "Value", collection, user);
+        CardEntity card2 = new CardEntity(2L, "Side", "Value", collection, user);
+        CardEntity card3 = new CardEntity(3L, "Side", "Value", collection, user);
+        collection.addCard(card1);
+        collection.addCard(card2);
+        collection.addCard(card3);
+
+        when(collectionRepository.findById(1L)).thenReturn(Optional.of(collection));
+        stubFor(get(urlEqualTo("/api/cards/all?=1"))
+                .willReturn(aResponse()
+                        .withStatus(200)));
+        CloseableHttpClient client = HttpClientBuilder.create().build();
+        HttpGet request = new HttpGet("http://localhost:8080/api/cards/all?=1");
+        CloseableHttpResponse response = client.execute(request);
+
+        assertEquals(200, response.getCode());
     }
 
 
