39af2c856850a2072e0ec1139f74073a6065a846
paweu9000 <wolfenorrin5@gmail.com>
2023-02-27 15:59:55 +0100

    collectionDto validation

--- flashback/src/main/java/com/flashcard/flashback/collection/data/CollectionDto.java
+++ flashback/src/main/java/com/flashcard/flashback/collection/data/CollectionDto.java
@@ -2,8 +2,15 @@ package com.flashcard.flashback.collection.data;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class CollectionDto {
+    @NotNull
+    @NotEmpty
+    @NotBlank
     private String title;
     private Long likes = 0L;
 }
