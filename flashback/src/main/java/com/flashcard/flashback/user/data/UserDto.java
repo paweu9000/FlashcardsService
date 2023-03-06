6b2b63806ffe36ba5fa660a50ff3be584f4f6c7d
paweu9000 <wolfenorrin5@gmail.com>
2023-03-06 14:06:02 +0100

    UserDto refactor

--- flashback/src/main/java/com/flashcard/flashback/user/data/UserDto.java
+++ flashback/src/main/java/com/flashcard/flashback/user/data/UserDto.java
@@ -2,6 +2,7 @@ package com.flashcard.flashback.user.data;
 
 import lombok.Builder;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.*;
 
@@ -11,14 +12,12 @@ public class UserDto {
     @NotBlank
     @NotNull
     @NotEmpty
-    @Min(4)
-    @Max(30)
+    @Length(min = 2, max = 30)
     private String login;
     @NotBlank
     @NotNull
     @NotEmpty
-    @Min(2)
-    @Max(30)
+    @Length(min = 2, max = 30)
     private String username;
     @NotNull
     @NotBlank
@@ -28,6 +27,6 @@ public class UserDto {
     @NotBlank
     @NotNull
     @NotEmpty
-    @Min(6)
+    @Length(min = 6)
     private String password;
 }
