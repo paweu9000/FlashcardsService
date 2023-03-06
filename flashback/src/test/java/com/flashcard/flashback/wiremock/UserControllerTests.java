5855b89b4f32c18c2923d6f32a3f3c7021e42206
paweu9000 <wolfenorrin5@gmail.com>
2023-03-06 12:07:04 +0100

    get user auth controller

---
+++ flashback/src/test/java/com/flashcard/flashback/wiremock/UserControllerTests.java
@@ -0,0 +1,65 @@
+package com.flashcard.flashback.wiremock;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.flashcard.flashback.user.data.UserDao;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserControllerTests {
+
+    private WireMockServer wireMockServer;
+    private UserDao userDao;
+    private ObjectMapper objectMapper;
+
+    @Before
+    public void setup() {
+        wireMockServer = new WireMockServer(8080);
+        wireMockServer.start();
+        userDao = new UserDao();
+        userDao.setId(1L);
+        userDao.setUsername("username");
+        userDao.setCollections(new ArrayList<>());
+        userDao.setSavedCollections(new ArrayList<>());
+        objectMapper = new ObjectMapper();
+    }
+
+    @After
+    public void tearDown() {
+        wireMockServer.stop();
+    }
+
+    @Test
+    @WithMockUser("login")
+    public void getUserDataValidTest() throws IOException, InterruptedException {
+        String body = objectMapper.writeValueAsString(userDao);
+
+        stubFor(get(urlEqualTo("/api/user/1"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withBody(body)));
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/api/user/1"))
+                .GET()
+                .build();
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        assertEquals(200, response.statusCode());
+        assertEquals(body, response.body());
+    }
+}
