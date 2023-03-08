29e8f335e53d63526c038877a41cb8143bf7ed03
paweu9000 <wolfenorrin5@gmail.com>
2023-03-08 14:38:16 +0100

    mapVerificationToken method

--- flashback/src/main/java/com/flashcard/flashback/verification/entity/VerificationToken.java
+++ flashback/src/main/java/com/flashcard/flashback/verification/entity/VerificationToken.java
@@ -3,7 +3,6 @@ package com.flashcard.flashback.verification.entity;
 import com.flashcard.flashback.user.entity.UsersEntity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
--- flashback/src/main/java/com/flashcard/flashback/verification/service/VerificationTokenService.java
+++ flashback/src/main/java/com/flashcard/flashback/verification/service/VerificationTokenService.java
@@ -18,13 +18,18 @@ public class VerificationTokenService {
 
     public VerificationToken generateVerificationToken(UsersEntity user) {
         String token = UUID.randomUUID().toString();
-        VerificationToken verificationToken = new VerificationToken();
-        verificationToken.setToken(token);
-        verificationToken.setUsersEntity(user);
+        VerificationToken verificationToken = mapToken(token, user);
         return repository.save(verificationToken);
     }
 
     public VerificationToken getVerificationToken(String token) {
         return repository.findByToken(token).get();
+    }
+
+    public VerificationToken mapToken(String token, UsersEntity usersEntity) {
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUsersEntity(usersEntity);
+        return verificationToken;
     }
 }
