1628a81d215836131af111fc0623284c13e0d32f
paweu9000 <wolfenorrin5@gmail.com>
2023-03-10 16:21:00 +0100

    emailService sendMessageTest

--- flashback/src/test/java/com/flashcard/flashback/emailtests/EmailServiceTests.java
+++ flashback/src/test/java/com/flashcard/flashback/emailtests/EmailServiceTests.java
@@ -7,8 +7,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class EmailServiceTests {
@@ -23,5 +29,14 @@ public class EmailServiceTests {
     public void mockNotNullTest() {
         assertNotNull(javaMailSender);
         assertNotNull(emailService);
+    }
+
+    @Test
+    public void sendMessageTest() throws MessagingException {
+        MimeMessage mimeMessage = mock(MimeMessage.class);
+        when(javaMailSender.createMimeMessage()).thenReturn(mimeMessage);
+        emailService.sendVerificationEmail("email@example.com", "token");
+
+        verify(javaMailSender).send(any(MimeMessage.class));
     }
 }
