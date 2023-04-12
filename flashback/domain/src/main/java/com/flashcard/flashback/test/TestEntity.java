fcc01165b28d0e4eddc1666d75eb2b98dd0ab805
paweu9000 <wolfenorrin5@gmail.com>
2023-04-12 21:07:24 +0200

    Initial QuestionEntity and TestEntity models

---
+++ flashback/domain/src/main/java/com/flashcard/flashback/test/TestEntity.java
@@ -0,0 +1,11 @@
+package com.flashcard.flashback.test;
+
+import com.flashcard.flashback.collection.CollectionEntity;
+
+import java.util.Set;
+
+class TestEntity {
+    private Long id;
+    private Set<QuestionEntity> questions;
+    private CollectionEntity collection;
+}
