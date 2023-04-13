2d207ccfafa3999de14ced2cce551d3f5c012bed
paweu9000 <wolfenorrin5@gmail.com>
2023-04-13 12:41:22 +0200

    create TestRepository

---
+++ flashback/domain/src/main/java/com/flashcard/flashback/test/TestRepository.java
@@ -0,0 +1,11 @@
+package com.flashcard.flashback.test;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TestRepository extends JpaRepository<TestEntity, Long> {
+    Optional<TestEntity> findByCollectionId(Long id);
+}
