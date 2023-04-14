3a657b4fe5041ea2a5f3418a7db7abf2ae778d5a
paweu9000 <wolfenorrin5@gmail.com>
2023-04-14 13:23:43 +0200

    initial placeholder for createTestEntity

--- flashback/app/src/main/java/com/flashcard/flashback/test/TestService.java
+++ flashback/app/src/main/java/com/flashcard/flashback/test/TestService.java
@@ -1,12 +1,21 @@
 package com.flashcard.flashback.test;
 
+import com.flashcard.flashback.collection.CollectionEntity;
+import com.flashcard.flashback.collection.CollectionService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TestService {
+class TestService {
     final TestRepository repository;
+    final CollectionService collectionService;
 
-    public TestService(TestRepository repository) {
+    public TestService(TestRepository repository, CollectionService collectionService) {
         this.repository = repository;
+        this.collectionService = collectionService;
+    }
+
+    TestEntity createTestEntity(Long collectionId) {
+        CollectionEntity collection = collectionService.findById(collectionId);
+        return null;
     }
 }
