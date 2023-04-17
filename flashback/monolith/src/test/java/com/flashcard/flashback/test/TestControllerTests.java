ee5227823e325053b7a588a2fb24fe51f50c406a
paweu9000 <wolfenorrin5@gmail.com>
2023-04-17 17:38:14 +0200

    get test valid

--- flashback/monolith/src/test/java/com/flashcard/flashback/test/TestControllerTests.java
+++ flashback/monolith/src/test/java/com/flashcard/flashback/test/TestControllerTests.java
@@ -5,6 +5,17 @@ import com.flashcard.flashback.test.data.TestDao;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestControllerTests {
 
@@ -25,5 +36,25 @@ public class TestControllerTests {
     @After
     public void tearDown() {
         wireMockServer.stop();
+    }
+
+    @Test
+    public void getTestValidTest() throws IOException, InterruptedException {
+        String body = objectMapper.writeValueAsString(testDao);
+
+        stubFor(get(urlEqualTo("/api/test/1"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withBody(body)));
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/api/test/1"))
+                .GET()
+                .build();
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        assertEquals(200, response.statusCode());
+        assertEquals(body, response.body());
     }
 }
